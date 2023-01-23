5fa65a32135263aeff32641917afa098a1651d22
X3llus <17010070+X3llus@users.noreply.github.com>
2023-01-23 11:09:06 -0500

    colour fix

--- website/tailwind.config.cjs
+++ website/tailwind.config.cjs
@@ -2,15 +2,16 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    colors: {
-      'primary': '#73C536',
-      'secondary': '#3686C9',
-      'background': '#E0E2D2',
-      'rick-black': '#0A1D29',
-      'independence': '#575A6C',
-      'dark-khaki': '#C3C786',
+    extend: {
+      colors: {
+        'primary': '#73C536',
+        'secondary': '#3686C9',
+        'background': '#E0E2D2',
+        'rick-black': '#0A1D29',
+        'independence': '#575A6C',
+        'dark-khaki': '#C3C786',
+      },
     },
-    extend: {},
   },
   plugins: [],
 }
