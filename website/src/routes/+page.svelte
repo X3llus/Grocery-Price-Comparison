26633b72a76cd28986d8abae2f2e39543ff638c3
X3llus <17010070+X3llus@users.noreply.github.com>
2023-01-25 14:57:17 -0500

    fixed carousel

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -1,33 +1,35 @@
 <script>
+	import { browser } from '$app/environment';
     import Carousel from 'svelte-carousel';
 
-    const nums = [1,2,3,4,5];
+	const nums = [1, 2, 3, 4, 5];
 </script>
+
 <div class="mx-auto text-center pt-20">
-    <h1 class="text-7xl text-gray-600">Groceriez</h1>
-    <p>
-        Tired of taking time and always looking for the best price when grocery shopping? Look no further. 
-        Groceriez is the go-to App for price comparison at your local chain, Ontarian grocers. 
-        Never overpay for groceries again, and simplify your shopping experience with our one-of-a-kind App!
-    </p>
-    <h2>Our Supported Stores</h2>
-    <div class="">
-        <!-- Image Carousel -->
+	<h1 class="text-7xl text-gray-600">Groceriez</h1>
+	<p>
+		Tired of taking time and always looking for the best price when grocery shopping? Look no
+		further. Groceriez is the go-to App for price comparison at your local chain, Ontarian grocers.
+		Never overpay for groceries again, and simplify your shopping experience with our one-of-a-kind
+		App!
+	</p>
+	<h2>Our Supported Stores</h2>
+	<div class="">
+		<!-- Image Carousel -->
+		{#if browser}
         <Carousel>
-            {#each nums as num}
+			{#each nums as num}
                 <div>{num}</div>
             {/each}
-            
-        </Carousel>
-
-    </div>
-    <p>
-        <!-- Copy about supported Stores -->
-    </p>
-    <h2>Search our Database Now!</h2>
-    <div class="">
-        <!-- Search bar -->
-    </div>
-    <!-- Coming features Copy? -->
+		</Carousel>
+        {/if}
+	</div>
+	<p>
+		<!-- Copy about supported Stores -->
+	</p>
+	<h2>Search our Database Now!</h2>
+	<div class="">
+		<!-- Search bar -->
+	</div>
+	<!-- Coming features Copy? -->
 </div>
-
--- website/vite.config.js
+++ website/vite.config.js
@@ -2,6 +2,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
+	optimizeDeps: {
+		include: ['lodash.get', 'lodash.isequal', 'lodash.clonedeep']
+	},
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
