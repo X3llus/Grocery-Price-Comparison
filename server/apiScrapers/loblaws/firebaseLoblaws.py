965449fc9fbc4735e2f684f62ef193f5841a1ab0
ndforler <ndforler@lakeheadu.ca>
2023-01-26 00:28:38 -0500

    Update firebaseLoblaws.py
    Added the code needed to create entries into firebase through python.

--- server/apiScrapers/loblaws/firebaseLoblaws.py
+++ server/apiScrapers/loblaws/firebaseLoblaws.py
@@ -2,6 +2,12 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
+# from loblawsScraper import 
+
 # Going to setup creds with firebase
-cred = credentials.Certificate()
+cred = credentials.Certificate("server\serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+# db.collection().add({}) mock code for adding documents to the database
