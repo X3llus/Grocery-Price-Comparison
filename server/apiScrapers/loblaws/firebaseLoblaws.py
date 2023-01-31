d7090500caef0ec3ead698461ffeb22f9cf1cdf1
Adam Brennan <atbrenna@lakeheadu.ca>
2023-01-31 13:31:41 -0500

    Basic unfinished version of adding products to firebase

--- server/apiScrapers/loblaws/firebaseLoblaws.py
+++ server/apiScrapers/loblaws/firebaseLoblaws.py
@@ -1,17 +1,33 @@
+import asyncio
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-from loblawsScraper import get_all_products, get_products
+from loblawsScraper import get_all_products
+from data import categories
 
+# from loblawsScraper import 
+# category = ["preparedMeals"]["meat"]["fishAndSeafood"]["fruitsAndVegetables"]["deli"]["bakery"]["dairyAndEggs"]["drinks"]["frozen"]["pantry"]["naturalFoods"]["beerAndWine"]["snacksChipsAndCandy"]["internationalFoods"]
 # Going to setup creds with firebase
 cred = credentials.Certificate("server\serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-# db.collection().add({}) mock code for adding documents to the database
-# def store_data(collection_id, data):
-#     db.collection(collection_id).add(data)
+products = asyncio.run(get_all_products())
 
-# store_data(collection_id = "Store", data = data.store_locations)
+# db.collection().add({}) mock code for adding documents to the database
+for u in range(0, len(products["fishAndSeafood"])):
+    print(products["fishAndSeafood"][u]["name"])
+    # head, sep, tail = categories[u].partition(':')
+    # print(head)
+    # for i in range(0, len(products[head])):
+    #     print(products[head][i]["name"])
+# print(type(products))
+# productsValue = list(products.values())
+# print(type(productsValue))
+# print(productsValue[1])
+# print(productsValue[1]["Name"])
+# db.collection('products').add({
+# products
+# })
