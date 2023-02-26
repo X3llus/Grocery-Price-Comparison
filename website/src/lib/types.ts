90dcb72f5f9321e0b7671d149d2cba21c30131de
DESKTOP-K20A7N9\fretz <fretztyler@gmail.com>
2023-02-26 15:57:39 -0500

    new(dev): add types

---
+++ website/src/lib/types.ts
@@ -0,0 +1,47 @@
+export type Product = {
+  sku: string
+  name: string
+  brand: string
+  imageUrl: string
+  packageSize: string
+  parentCompany: string
+}
+
+export type NormalizedPrice = {
+  quantity: number
+  unit: string
+  value: number
+}
+
+export type ProductPrice = {
+  key: string
+  dateExtracted: string
+  inStock: boolean
+  normalizedPrice: NormalizedPrice
+  price: number
+}
+
+export type ProductWithPrice = Product & ProductPrice
+
+export type GeoPoint = {
+  latitude: number
+  longitude: number
+}
+
+export type StoreAddress = {
+  line1: string
+  town: string
+  region: string
+  postcode: string
+  country: string
+  formattedAddress: string
+}
+
+export type Store = {
+  storeId: string
+  type: string
+  address: StoreAddress
+  geoPoint: GeoPoint
+}
+
+export { }
