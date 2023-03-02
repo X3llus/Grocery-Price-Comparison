f623c7e783e43dba23a80c5e7fbd946b1cbbc5a1
DESKTOP-K20A7N9\fretz <fretztyler@gmail.com>
2023-03-02 18:43:49 -0500

    change search input in search page to match style of landing page. Add aria labels

--- website/src/routes/search/+layout.svelte
+++ website/src/routes/search/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { onMount } from 'svelte';
 	import { page } from '$app/stores';
+	import Magnify from 'svelte-material-icons/Magnify.svelte';
 	import Cart from 'svelte-material-icons/Cart.svelte';
 	import MapMarker from 'svelte-material-icons/MapMarker.svelte';
 	import Modal from '$lib/Modal.svelte';
@@ -9,6 +10,7 @@
 
 	$: q = $page.url.searchParams.get('q') || '';
 
+	let search = '';
 	let sideOpen = false;
 	let locationModalOpen = false;
 
@@ -20,6 +22,10 @@
 
 	const toggleLocationModal = () => {
 		locationModalOpen = !locationModalOpen;
+	};
+
+	const performSearch = () => {
+		console.log("example product search for: " + search);
 	};
 
 </script>
@@ -43,7 +49,7 @@
 {/if}
 
 <!-- Main Nav Bar -->
-<div class="fixed left-0 top-0 w-screen z-10 p-4 flex space-x-4 bg-primary shadow-lg">
+<div class="fixed left-0 top-0 w-screen z-10 p-1 md:p-4 flex space-x-2 md:space-x-4 bg-primary shadow-lg">
 	<!-- <button on:click={() => (sideOpen = !sideOpen)}>
 		<div class="space-y-2">
 			<div class="w-8 h-1 bg-white rounded-sm" />
@@ -53,24 +59,38 @@
 	</button> -->
 	<h1 class="text-3xl text-white leading-normal"><a href="/">Groceriez</a></h1>
 	<div class="flex flex-1">
-		<input class="p-2 rounded-lg flex-1" placeholder="Search products" />
+		<form 
+			on:submit|preventDefault={() => performSearch()}
+			class="flex p-2 rounded-lg flex-1"
+		>
+			<input 
+					bind:value={search}
+					class="p-2 rounded-l-lg w-3/4 border-y border-l-2 border-accent shadow-md bg-gradient-to-b focus:outline-none"
+					placeholder="Search our Products"
+			/>
+			<button type="submit" class="p-2 rounded-r-lg border-y border-r-2 border-accent bg-accent bg-grad shadow-md w-16 flex justify-center">
+					<Magnify color={'white'} width={24} height={24} />
+			</button>
+		</form>
 		<div class="flex flex-col justify-center">
-			<div
+			<button
 				class="flex-initial mx-14 flex hover:cursor-pointer text-white font-medium"
 				on:click|stopPropagation={toggleLocationModal}
 				on:keypress|stopPropagation={toggleLocationModal}
+				aria-label="Change Location"
 			>
 				<div class="flex flex-col justify-center"><MapMarker width={22} height={22} /></div>
-				<div class="flex flex-col">
+				<div class="flex flex-col items-start">
 					<span>{$userLocation.city}, {$userLocation.province}</span>
 					<span class="text-xs">Change Location</span>
 				</div>
-			</div>
+			</button>
 		</div>
 	</div>
 	<button
 		class="rounded-full bg-white w-12 h-12 flex justify-center items-center"
 		on:click={() => (sideOpen = !sideOpen)}
+		aria-label="Cart"
 	>
 		<Cart color={'black'} width={24} height={24} />
 	</button>
