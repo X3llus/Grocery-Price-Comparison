e7ef462dd01eca0646223b4e400956d931cd8ad2
ndforler <ndforler@lakeheadu.ca>
2023-03-04 14:30:43 -0500

    Update test_utils.py
    Testing distance between coordinates
    Created a test for testing both units 'N' and 'K' (miles, and kilometers).

--- server/tests/test_utils.py
+++ server/tests/test_utils.py
@@ -1,14 +1,18 @@
-import math
 import sys
 sys.path.insert(0, '../')
 # import utils
 from utils import distance_between_coords, find_latitude_longitude_range, get_closest_store, format_loblaws_store
 
 
-def test_distance_between_coords():
+def test_distance_between_coords_km():
     x = distance_between_coords(10,11,12,12,"K")
-    # used online calculator to calculate the distance between coordinates
+    # used online calculator to calculate the distance between coordinates kilometers
     assert round(x) == 248 
+
+def test_distance_between_coords_n_mi():
+    x = distance_between_coords(10,11,12,12,"N")
+    # used online calculator to calculate the distance between coordinates miles
+    assert round(x) == 134 
 
 def test_find_latitude_longitude_range():
     pass
