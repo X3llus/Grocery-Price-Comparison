a1549bf4150ab8d0257da24e17922a7eefb207bd
ndforler <ndforler@lakeheadu.ca>
2023-03-04 16:54:30 -0500

    Update test_utils.py
    Wrote tests for formatting the loblaws stores, both valid and invalid inputs

--- server/tests/test_utils.py
+++ server/tests/test_utils.py
@@ -3,7 +3,7 @@ sys.path.insert(0, '../')
 # import utils
 from utils import distance_between_coords, find_latitude_longitude_range, get_closest_store, format_loblaws_store
 
-
+# testing how far two different points are from one another
 def test_distance_between_coords_km():
     x = distance_between_coords(10,11,12,12,"K")
     # used online calculator to calculate the distance between coordinates kilometers
@@ -20,5 +20,41 @@ def test_find_latitude_longitude_range():
 def test_get_closest_store():
     pass
 
-def test_format_loblaws_store():
-    pass
+def test_format_loblaws_store_valid():
+    # Test valid input
+    store = {
+        'storeId': '1234',
+        'storeBannerId': 'Loblaws',
+        'address': {
+            'country': 'Canada',
+            'line1': '123 Main St',
+            'postalCode': 'M1M1M1',
+            'town': 'Toronto',
+            'region': 'Ontario'
+        },
+        'geoPoint': {
+            'latitude': 43.6532,
+            'longitude': -79.3832
+        }
+    }
+    expected_output = {
+        'address': {
+            'country': 'Canada',
+            'formattedAddress': '123 Main St Toronto, Ontario M1M1M1',
+            'line1': '123 Main St',
+            'postalCode': 'M1M1M1',
+            'region': 'Ontario',
+            'town': 'Toronto'
+        },
+        'geoPoint': {
+            'latitude': 43.6532,
+            'longitude': -79.3832
+        },
+        'storeId': '1234',
+        'type': 'Loblaws'
+    }
+    assert format_loblaws_store(store) == expected_output
+
+def test_format_loblaws_store_invalid():
+    store = {}
+    assert format_loblaws_store(store) is None
