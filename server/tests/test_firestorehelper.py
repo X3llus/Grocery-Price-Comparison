e2d0d210f256b790722d4b357119214884bd3018
ndforler <ndforler@lakeheadu.ca>
2023-03-05 16:13:35 -0500

    Update test_firestorehelper.py
    Defined all tests required for FirestoreHelper

--- server/tests/test_firestorehelper.py
+++ server/tests/test_firestorehelper.py
@@ -1,2 +1,15 @@
 import sys
 sys.path.insert(0, '../')
+from FirestoreHelper import FirestoreHelper
+
+def test_get_local_stores():
+    pass
+
+def test_get_local_stores_postal():
+    pass
+
+def test_add_loblaws_stores():
+    pass
+
+def test_add_walmart_stores():
+    pass
