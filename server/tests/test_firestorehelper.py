de8afc4d35a88e8061b19c25d7eab0970904fac4
ndforler <ndforler@lakeheadu.ca>
2023-03-05 21:36:19 -0500

    Update test_firestorehelper.py
    Finished testing get_local_stores in the fireStoreHelper script

--- server/tests/test_firestorehelper.py
+++ server/tests/test_firestorehelper.py
@@ -1,10 +1,43 @@
 import sys
 sys.path.insert(0, '../')
-from FirestoreHelper import FirestoreHelper
-#check firestoreHelper 
+from FirestoreHelper import FirestoreHelper as FH
 
 def test_get_local_stores():
-    pass
+    ins = FH()
+    expectedValue = [
+        {
+            'address': {
+                'town': 'Orillia', 
+                'region': 'ON', 
+                'line1': '175 Murphy Rd', 
+                'formattedAddress': '175 Murphy Rd Orillia, ON L3V 0B5', 
+                'postalCode': 'L3V 0B5', 
+                'country': 'CA'
+            }, 
+            'geoPoint': {
+                'latitude': 44.61571, 
+                'longitude': -79.445723
+            }, 
+            'storeId': '3153', 
+            'type': 'Walmart'
+        }, 
+        {
+            'address': {
+                'town': 'Orillia', 
+                'region': 'Ontario', 
+                'line1': '289 Coldwater Road', 
+                'formattedAddress': '289 Coldwater Road Orillia, Ontario L3V 6J3', 'postalCode': 'L3V 6J3', 
+                'country': 'Canada'
+            },
+            'geoPoint': {
+                'latitude': 44.608261, 
+                'longitude': -79.437689
+            },
+            'storeId': '0580',
+            'type': 'zehrs'
+        }]
+    print(ins.get_local_stores(44.608261,-79.437689,1))
+    assert ins.get_local_stores(44.608261,-79.437689,1) == expectedValue
 
 def test_get_local_stores_postal():
     pass
