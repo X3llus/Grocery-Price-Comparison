7b40ab76a9a6b4e51a72572afba1b78e1ee3cf4d
ndforler <ndforler@lakeheadu.ca>
2023-04-02 14:26:41 -0400

    Finding the first store
    Determined the first store that is being cached into Firestore. the store document id is going to be used to ensure all of the products from this store are added into firestore, without checking the name, size or brand.

--- scrapers/main.py
+++ scrapers/main.py
@@ -15,10 +15,12 @@ def main():
   stores = firestore_helper.get_local_stores(lat, lng, radius)
   num_stores = len(stores)
   print(f'\nFound {num_stores} stores within {radius}km of {lat}, {lng}.\n')
+  print(stores[0])
+
   
-  scrapy_run(stores)
+  # scrapy_run(stores)
   
-  print(f'\nAll stores complete. Total time: {time.time() - start_time} seconds.')
+  # print(f'\nAll stores complete. Total time: {time.time() - start_time} seconds.')
 
   
 if __name__ == '__main__':
