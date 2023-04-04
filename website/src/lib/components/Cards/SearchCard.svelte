4d56399d1f3bd97e8239ddf38c45854a6805c194
jona8780 <jona8780@users.noreply.github.com>
2023-04-03 23:31:38 -0400

    Populate store pricing on cards

--- website/src/lib/components/Cards/SearchCard.svelte
+++ website/src/lib/components/Cards/SearchCard.svelte
@@ -14,7 +14,7 @@
 
 <div class="w-full border rounded-md shadow-lg overflow-hidden group bg-white flex flex-col justify-between">
     <img src={hit.imageUrl} alt={hit.name} class="w-full" />
-    <div class="p-5">
+    <div class="px-5 pt-5">
         <h2
             class="truncate text-primary text-xl font-sans font-medium hover:text-black"
             data-tooltip-target="title"
@@ -32,7 +32,6 @@
                 {hit.brand}
             </h2>
         {/if}
-        <div class="border m-1" />
         <!-- <div class="p-1">
             <div class="flex justify-between">
                 <h3 class="text-rich-black font-medium text-md">Store</h3>
@@ -47,12 +46,19 @@
                 </div>
             </div>
         {/if}
+        <div class="border m-1" />
         <div class="p-1">
-            <div class="flex justify-between">
-                <h3 class="text-rich-black font-medium text-md">Price</h3>
-                <h3 class="text-rich-black font-medium text-md">${hit.data[0].price}</h3>
-            </div>
+            {#each hit.data.sort((a, b) => a.price - b.price) as data, i}
+                {#if i < 3}
+                    <div class="flex justify-between">
+                        <h3 class="{ i === 0 ? 'text-primary font-bold' : 'text-rich-black font-medium' } text-md capitalize truncate" title="{hit.data[i].storeName}">{hit.data[i].storeName}</h3>
+                        <h3 class="{ i === 0 ? 'text-primary font-bold' : 'text-rich-black font-medium' } text-md">${hit.data[i].price}</h3>
+                    </div>
+                {/if}
+            {/each}
         </div>
+    </div>
+    <div class="px-5 pb-5">
         <button
             class="button bg-primary w-full rounded-xl text-white text-sm font-sans font-medium p-2 flex justify-center shadow-md hover:opacity-90 focus:animate-wiggle"
             on:click={() => addProduct(i)}>Add to List</button
