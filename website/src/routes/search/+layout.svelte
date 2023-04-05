cdeaa2680de41bbeb65549d9a5a6754a2949e2db
DESKTOP-K20A7N9\fretz <fretztyler@gmail.com>
2023-04-05 16:22:49 -0400

    (fix): add check to make sure searchListStore is an array

--- website/src/routes/search/+layout.svelte
+++ website/src/routes/search/+layout.svelte
@@ -203,10 +203,12 @@
 		aria-label="Cart"
 	>
 		<ListBox color={'white'} width={24} height={24} />
-		<div
-			class="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -right-2 dark:border-gray-900"
-		>
-			{$searchListStore.reduce((acc, curr) => acc + curr.quanity, 0)}
-		</div>
+		{#if Array.isArray($searchListStore) && $searchListStore.length > 0}
+			<div
+				class="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -right-2 dark:border-gray-900"
+			>
+				{$searchListStore.reduce((acc, curr) => acc + curr.quanity, 0)}
+			</div>
+		{/if}
 	</button>
 </div>
